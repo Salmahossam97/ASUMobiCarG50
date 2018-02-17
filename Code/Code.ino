8eef4b47b7ba96be4bd723c229214a21e38ccadf
Salma Tarek <1500667@eng.asu.edu.eg>
2018-02-17 22:42:23 +0200

    1st trial for the Left button #3

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,10 +1,40 @@
+char order;
+int A1 = ; //A1 is the 1st right motors pin
+int B1 = ; //B1 is the 2nd right motors pin
+int A2 = ; //A2 is the 1st left motors pin
+int B2 = ; //B2 is the 2nd left motors pin
+
 void setup() {
-  // put your setup code here, to run once:
-  // besmallah
+
+  pinMode(ENA, OUTPUT); //Enable pins
+  pinMode(ENB, OUTPUT); //Enable pins
+  pinMode(A1, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B2, OUTPUT);
+
+}
+
+void Left () {
+  
+  analogWrite(ENA, 255); //adjusting the speed to the highest value
+  analogWrite(ENB, 255); //adjusting the speed to the highest value
+  //turning the right motors off
+  digitalWrite(A1, LOW); 
+  digitalWrite(B1, LOW);
+  //turning the left motors on
+  digitalWrite(A2, HIGH);
+  digitalWrite(B2, LOW);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+ order = Serial.read(); 
+
+ if (order == 'L')
+ {
+  Left();
+ }
 
 }
